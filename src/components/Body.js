09e39b3b510c9d8be95f02450f83771aad15fe59
saranyamaity <saranyamaity2000@gmail.com>
2024-10-27 01:34:36 +0530

    clean body component with search on click and filter at the side also

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,38 +6,39 @@ import { zomatoResponse } from '../utils/data';
 import Card from './Card';
 import { useEffect, useState } from "react";
 
-let renderCount = 0;
 
 const Body = () => {
-    const [resturants, setResturants] = useState(zomatoResponse.sections.SECTION_SEARCH_RESULT);
+    const restaurants = zomatoResponse.sections.SECTION_SEARCH_RESULT;
+
     const [filterOn, setFilterOn] = useState(false);
     const [searchText, setSearchText] = useState("");
+    const [searchWithText, setSearchWithText] = useState(searchText);
+
+
 
     const filterClickHandler = () => setFilterOn(!filterOn);
     const onSearchInput = (e) => setSearchText(e.target.value);
+    const onSearchClick = () => setSearchWithText(searchText);
 
-    const getFilteredResturant = () => { // will return the desired filtered list (it will update every time its dependency updates)
-        return resturants
-            .filter(res => res.info.name.toLowerCase().includes(searchText.toLowerCase())) // search Filter 
-            .filter(res => (!filterOn || +res.info.rating.aggregate_rating >= 4))
-    };
+    const filteredRes = restaurants.filter(res => {
+        const matchesRating = !filterOn || res.info.rating.aggregate_rating > 4;
+        const matchesSearch = !searchWithText || res.info.name.toLowerCase().includes(searchWithText.toLowerCase());
+        return matchesRating && matchesSearch;
+    });
 
-    renderCount++;
-    useEffect(() => console.log("Hello!"), []);
     return (
         <div>
             <div className="search-container">
-                <FontAwesomeIcon icon={faMagnifyingGlass} className="icon mg"/>
+                <FontAwesomeIcon icon={faMagnifyingGlass} className="icon mg" onClick={onSearchClick}/>
                 <input className="input-search-bar" onInput={onSearchInput}/>
                 <FontAwesomeIcon icon={faFilter} 
                     className="icon filter" 
                     onClick={filterClickHandler} 
                 />
             </div>
-            <p>{renderCount}</p>
             <div className="card-container">
                 {
-                    getFilteredResturant()
+                    filteredRes
                         .map((res, idx) => <Card key={res.info.resId}  resName={res.info.name} resRating={res.info.rating.aggregate_rating} resCuisine={res.info.cuisine} resImg={res.info.image}></Card>)
                 }
             </div>
