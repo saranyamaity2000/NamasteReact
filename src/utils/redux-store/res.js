c8e0e79418844e92eb158632dd9974192d8b468e
saranyamaity <saranyamaity2000@gmail.com>
2024-11-07 00:47:30 +0530

    final change

--- src/utils/redux-store/res.js
+++ src/utils/redux-store/res.js
@@ -11,7 +11,11 @@ const resSlice = createSlice({
         },
         removeFromFavourite(state, action) {
             state.favRes = state.favRes.filter(res => res != action.payload);
-            // immutable change this also works
+            // immutable change this also works (but still mutable on favRes layer)
+
+            state = {favRes: ["default"]} ; // this line has no meaning.
+            // this wont work as we are returning this state , no assing state like this
+            // is nothing.
         }
     }
 });
